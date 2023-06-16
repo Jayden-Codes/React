9d1dc92eb1562db1e727189a8271e678a2662b55
Jayden Abrahams <jabrahamsx@gmail.com>
2023-06-16 23:30:42 +0200

    content add

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import "../CSS/card.css";
 import pic from "../images/download.jpeg";
 const Card = () => {
+  const [quantity, setQuantity] = useState(0,);
   return (
     <div className="card">
       <div className="cardImageContainer">
         <img src={pic} alt="image" srcset="" />
+        <button className="cardAddBtn" onClick={ ()=> setQuantity(quantity+1)}>{quantity}</button>
       </div>
+
       <div className="cardText">
         <h2> Shrek 1</h2>
         <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora omnis quisquam vel vero quas pariatur blanditiis fugiat aliquam sint ad autem praesentium, ab dolor quos et, illum incidunt fugit recusandae!</h4>
